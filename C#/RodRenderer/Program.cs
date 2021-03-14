e6a24b3f4d7ba2b4d19fbce8446f983112f7c334
RodroVMS <democlesvanshepars@gmail.com>
2021-03-14 18:35:35 -0400

    Created RodRenderer folder for project development

---
+++ C#/RodRenderer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RodRenderer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
